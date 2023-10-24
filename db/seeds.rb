6ae15d25897321f2d4ad3ce2abfca6bd1dfd9b95
yisroelgugenheim <126514996+yisroelgugenheim@users.noreply.github.com>
2023-10-23 21:29:31 -0500

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Contact.create(first_name: "Santa", last_name: "Claus", email: "saintnick@test.com", phone_number: "2225551225")
+Contact.create(first_name: "Easter", last_name: "Bunny", email: "hippityhop@test.com", phone_number: "22255550312")
+Contact.create(first_name: "Patrick", last_name: "of Ireland", email: "luckyguy@test.com", phone_number: "22255550317")
+Contact.create(first_name: "Arbor Day", last_name: "Jackson", email: "treelover@test.com", phone_number: "22255550422")
